6f5050095adf47ea71c8b37e7ae5fbc3fecf6dd9
Wesley Reynafarje <wesley.reynafarje@mrjaffesclass.com>
2023-02-22 12:47:41 -0800

    Finished to step 7

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="chat-container">
+      <div className="chat-username">Username</div>
+      <div className="chat-log">Chat log</div>
+      <div className="chat-message-input">Message input</div>
+    </div>
+  );
+}
+
+export default App;
